d054715d0e5412d9f2a61f1639c73c90cdebe022
eba <goodgameeba@gmail.com>
2025-04-02 12:26:22 +0900

    商品用

--- database/migrations/2025_04_02_025955_create_products_table.php
+++ database/migrations/2025_04_02_025955_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,8 +12,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('products',function(Blueprint $table){
             $table->id();
+            $table->string('title',2000);
+            $table->string('slug',2000);
+            $table->string('description')->nullable();
+            $table->foreignId('department_id')->index->constrained('departments');
+            $table->foreignId('category_id')->index->constrained('categories');
+            $table->decimal('price',20,4);
+            $table->string('status')->default('');
+            $table->integer('quantity')->default('');
+            $table->foreignIdFor(User::class,'created_by');
+            $table->foreignIdFor(User::class,'updated_by');
+            $table->timestamp('deleted_at')->nullable();
             $table->timestamps();
         });
     }
